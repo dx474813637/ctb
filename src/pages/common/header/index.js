d0cb220aad808b256ee051da9a862a15d687c142
dx474813637 <474813637@qq.com>
2021-02-21 20:19:51 +0800

    20210221

---
+++ src/pages/common/header/index.js
@@ -0,0 +1,17 @@
+// 头部的js代码
+import $ from "jquery";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "@/assets/global.pcss";
+import "./index.css";
+
+let pathname = location.pathname;
+if (pathname === "/") {
+  pathname += "index.html";
+}
+$(".header-nav a").each((i, ele) => {
+  const href = $(ele).attr("href");
+  if (pathname === href) {
+    ele.classList.add("header-nav-active");
+  }
+});
