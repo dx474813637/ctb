f843e1f6fc1c12917482cb2eb4a4ccf23c97498a
dx474813637 <474813637@qq.com>
2021-02-22 17:30:20 +0800

    20210222 1730

--- src/pages/common/header/index.js
+++ src/pages/common/header/index.js
@@ -4,14 +4,23 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "@/assets/global.pcss";
 import "./index.css";
-
-let pathname = location.pathname;
-if (pathname === "/") {
-  pathname += "index.html";
-}
-$(".header-nav a").each((i, ele) => {
-  const href = $(ele).attr("href");
-  if (pathname === href) {
-    ele.classList.add("header-nav-active");
-  }
-});
+let i 
+$('#menu li').hover(function() {
+  // if(!$(this).hasClass('active')) {
+    $(this).addClass('active').siblings().removeClass('active')
+    var tarLi = $('#headerList .list-wrap').eq($(this).index())
+    tarLi.fadeIn().siblings().hide()
+  // }
+}, function() {
+  $('#headerList .list-wrap').eq($(this).index()).hide()
+})
+$('#menu').hover(function(){}, function() {
+  $('#menu li').removeClass('active')
+  $('#headerList .list-wrap').hide()
+})
+$('#headerList .list-wrap').hover(function() {
+  $(this).show()
+}, function() {
+  $('#menu li').removeClass('active')
+  $(this).hide()
+})
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,42 @@
 // 首页使用的js
+import Swiper, { Pagination, Autoplay, EffectFade } from "swiper";
+import "swiper/swiper-bundle.css";
 
 // 引入公共css
 import "bootstrap/dist/css/bootstrap.css";
 import "../common/reset.css";
-import "./index.css";
+import "./index.pcss";
 import "@/assets/global.pcss";
 
 // 引入页面公共部分的js
 import "../common/header";
 import "../common/footer";
 
-console.log("首页");
+// console.log("首页");
+Swiper.use([Pagination, Autoplay, EffectFade]); // 使用需要的功能
+const mySwiper = new Swiper("#swiper .swiper-container", {
+	pagination: {
+		el: ".swiper-pagination", // 分页器
+	},
+	loop: true,
+	effect: "fade",
+	fadeEffect: {
+		crossFade: true,
+	},
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+});
+const mySwiperTbtj = new Swiper("#tbtj .swiper-container", {
+	slidesPerView: 4,
+	spaceBetween: 20,
+	pagination: {
+		el: ".swiper-pagination", // 分页器
+	},
+	loop: true,
+	// autoplay: {
+	// 	delay: 10000,
+	// 	disableOnInteraction: false,
+	// },
+});
